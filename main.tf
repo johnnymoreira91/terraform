d77d724200f528adeb37671b17569a29f9c39da3
Johnny Moreira <jmoreira@engebras.com.br>
2024-04-07 16:01:26 -0300

    add vpc

--- main.tf
+++ main.tf
@@ -13,12 +13,68 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc_dev"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "dev_public_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "dev_igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "dev_igw"
+  }
+}
+
+resource "aws_route_table" "dev_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev_igw.id
+  }
+
+  tags = {
+    Name = "dev_route_table_rt"
+  }
+}
+
+resource "aws_route" "dev_routetointernet" {
+  route_table_id            = aws_route_table.dev_route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.dev_igw.id
+}
+
+resource "aws_route_table_association" "dev_pub_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.dev_route_table.id
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-051f8a213df8bc089"
   instance_type = "t2.micro"
   key_name = "key-personal"
+  user_data = <<-EOF
+                  #!/bin/bash
+                  cd /home/ubuntu
+                  echo "<h1>Pagina test</h1>" > index.html"
+                  nohup busybox  httpd -f -p 8080 &
+                 EOF
 
   tags = {
-    Name = "ecs_server_test"
+    Name = "ecs_terraform"
   }
 }
