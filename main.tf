cf1c7ea3f6b7787431f25ced868dea6a85af3739
Johnny Moreira <jmoreira@engebras.com.br>
2024-04-07 18:54:06 -0300

    change

--- main.tf
+++ main.tf
@@ -23,55 +23,108 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+ 
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+ 
+ tags = {
+   Name = "Project VPC IG"
+ }
+}
+
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ 
+ tags = {
+   Name = "2nd Route Table"
+ }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.second_rt.id
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
 
   tags = {
-    Name = "dev_public_subnet"
+    Name = "allow_http"
   }
 }
 
-resource "aws_internet_gateway" "dev_igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "dev_igw"
-  }
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
-resource "aws_route_table" "dev_route_table" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.dev_igw.id
-  }
-
-  tags = {
-    Name = "dev_route_table_rt"
-  }
-}
-
-resource "aws_route" "dev_routetointernet" {
-  route_table_id            = aws_route_table.dev_route_table.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                = aws_internet_gateway.dev_igw.id
-}
-
-resource "aws_route_table_association" "dev_pub_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.dev_route_table.id
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-051f8a213df8bc089"
+  subnet_id = aws_subnet.public_subnets.id
+  vpc_security_group_ids = [
+    aws_security_group.allow_http.id
+  ]
   instance_type = "t2.micro"
   key_name = "key-personal"
   user_data = <<-EOF
                   #!/bin/bash
-                  cd /home/ubuntu
-                  echo "<h1>Pagina test</h1>" > index.html"
-                  nohup busybox  httpd -f -p 8080 &
+                  apt update
+                  apt install -y nginx
+                  service nginx start
                  EOF
 
   tags = {
